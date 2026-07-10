ea8f9e1753b9cbe2483d98d33d962548ab0754a2
tirthosarkar <tirthosarkar1@gmail.com>
2026-07-10 13:53:38 +0600

    Added undefined.js

---
+++ undefined.js
@@ -0,0 +1,8 @@
+let data;
+// console.log(typeof data) // undefined
+
+const sum = (a, b) => {
+  console.log(a + b);
+}
+sum(4, 5) // 9
+// data1 = 5; // TypeError: Assignment to constant variable. 
