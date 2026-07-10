aa598ae2b76afffb3b74b51389553e09207f2c53
tirthosarkar <tirthosarkar1@gmail.com>
2026-07-10 13:59:03 +0600

    Added undefined.js delete arr[1]

--- undefined.js
+++ undefined.js
@@ -13,4 +13,12 @@ const student = {
   age: 22
 };
 console.log(student.name) // Tormuj
-console.log(student.marks) // undefined
+// console.log(student.marks) // undefined
+
+const arr = [1, 2, 3, 4];
+// console.log(arr[0]) // 1
+// console.log(arr[4]) // undefined
+
+delete arr[1]; // delete the element at index 1
+console.log(arr) // [ 1, <1 empty item>, 3, 4 ]
+
