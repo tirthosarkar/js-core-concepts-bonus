3a3e2120ee672e7d591d104c2b36a05206b47f19
tirthosarkar <tirthosarkar1@gmail.com>
2026-07-11 17:18:17 +0600

    Added double-equals.js true false

--- double-equals.js
+++ double-equals.js
@@ -1,2 +1,7 @@
 console.log(2 == 2); // true
+console.log(2 == '2'); // true
+console.log(2 == 3); // false
+console.log(2 == true); // false
+console.log(2 == false); // false
+console.log(2 == 3); // false
 console.log(2 == 3); // false
