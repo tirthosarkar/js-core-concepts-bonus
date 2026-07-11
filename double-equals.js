b63f92e19d375b88ec4ee1205c1a93361b43e30f
tirthosarkar <tirthosarkar1@gmail.com>
2026-07-11 21:28:38 +0600

    Added double-equals.js NaN == NaN

--- double-equals.js
+++ double-equals.js
@@ -8,3 +8,6 @@ console.log(2 == '1'); // false
 console.log(true == 1); // true
 console.log(false == 0); // true
 console.log(null == undefined); // true
+console.log(null == 0); // false
+console.log(undefined == 0); // false
+console.log(NaN == NaN); // false
