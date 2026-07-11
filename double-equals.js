fd81f3d7314bf13e493a58ce1f01813864e810bc
tirthosarkar <tirthosarkar1@gmail.com>
2026-07-11 17:21:28 +0600

    Added double-equals.js true false 1 false

--- double-equals.js
+++ double-equals.js
@@ -4,5 +4,6 @@ console.log(2 == 3); // false
 console.log(2 == true); // false
 console.log(2 == false); // false
 console.log(2 == 3); // false
-console.log(2 == 3); // false
 console.log(2 == '1'); // false
+console.log(true == 2); // false
+console.log(false == 2); // false
