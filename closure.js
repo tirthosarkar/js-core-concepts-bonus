ddb2372d5bb23f779f81e5ea324dcf6754fa5cb7
tirthosarkar <tirthosarkar1@gmail.com>
2026-07-11 22:38:19 +0600

    Added closure.js finish

--- closure.js
+++ closure.js
@@ -9,15 +9,29 @@ const outer = outerFunction();
 // console.log('in the outside:', outer); // Output: [Function: innerFunction]
 outer(); // Output: Hello from innerFunction!
 
-function counter() {
+function counter(owner) {
   let count = 0; // Local variable
   function increment() {
     count = count + 1; // Increment the count variable
   
-    console.log('value of count:', count); // Output: 0, 1, 2, ...
+    console.log('value of count:',owner, count); // Output: 0, 1, 2, ...
   }
   return increment;
 }
-const count1 = counter();
-count1(); // Output: 0
-count1();
+
+const rahimCounter = counter('Rahim');
+rahimCounter();
+rahimCounter();
+rahimCounter();
+rahimCounter();
+const karimCounter = counter('Karim');
+karimCounter();
+karimCounter();
+karimCounter();
+rahimCounter();
+rahimCounter();
+rahimCounter();
+
+// const count1 = counter();
+// count1(); // Output: 0
+// count1();
