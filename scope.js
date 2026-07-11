6ed9eb8e6ac7bffee74684ae0e2d77a05adab236
tirthosarkar <tirthosarkar1@gmail.com>
2026-07-11 21:54:43 +0600

    Added scope.js add(2,5) addTwo(num)

--- scope.js
+++ scope.js
@@ -8,7 +8,15 @@ function calculateArea(radius) {
 
 function add(a, b) {
   const factor = 0.5;
-  const result = a + b * factor+pi; // Local variable
+  const result = a + b * factor + pi; // Local variable
+  const total = doubleIt(result); // Call to another function
+  return total;
+  const value = addTwo(total); // Call to another function  
+
+  function = addTwo(num) {
+    return num + 2;
+  }
+
   return result;
 }
 add(5, 10); // Output: 5.64159
