2d1d46460fdfaf0433e60b59ca371a0968949f9f
tirthosarkar <tirthosarkar1@gmail.com>
2026-07-11 14:54:03 +0600

    Added truthy.js data = 0; data = ' '; data = 0; data = ' '; data = false; data = true; data = null; data = undefined; data = {}; data = [];

--- truthy.js
+++ truthy.js
@@ -1,8 +1,25 @@
 let data;
 data = 0;
-data= '';
+data = ' ';
+data = 0;
+data = ' ';
+data = false;
+data = true;
+data = null;
+data = undefined;
+data = {};
+data = [];
+
+
 if (data) {
   console.log('Truthy');
 } else {
   console.log('Falsy');
+}
+
+let price = 22; 
+if (price) {
+  console.log('Price is Truthy');
+} else {
+  console.log('Price is Falsy'); 
 }
