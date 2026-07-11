be062cdb0dae329128fce6c42726cd45661f3b95
tirthosarkar <tirthosarkar1@gmail.com>
2026-07-11 16:49:57 +0600

    Added double-equals.js

---
+++ double-equals.js
@@ -0,0 +1,2 @@
+console.log(2 == 2); // true
+console.log(2 == 3); // false
--- equals.js
+++ equals.js
@@ -1,1 +1,5 @@
-consol.log(2===2);
+console.log(2 === 2); // true
+console.log(2 === '2'); // false
+console.log(2===3); // false
+console.log(2 !== 3); // true 
+// compares the data type first . if data type are different it will return false. if data type are same it will compare the value and return true or false.
