cf744f6ff99ed54b031579126e13284dfb4c043d
tirthosarkar <tirthosarkar1@gmail.com>
2026-07-11 21:46:45 +0600

    Added scope.js factor, pi

--- scope.js
+++ scope.js
@@ -1,6 +1,14 @@
+let pi = 3.14159; // Global variable
+
+function calculateArea(radius) {
+  let area = pi * radius * radius; // Local variable
+  return area;
+}
+
 function add(a, b) {
   const factor = 0.5;
-  const result = a + b * factor;
+  const result = a + b * factor+pi; // Local variable
   return result;
 }
-console.log(factor)
+add(5, 10); // Output: 5.64159
+console.log(add(factor, pi)); // Output: 5.64159
