b1a3d61e8acb85eee099349d0b7565466a8eb3f7
tirthosarkar <tirthosarkar1@gmail.com>
2026-07-12 23:20:05 +0600

    added map.js const doubled = numbers.map(num => num * 2); squared friends

--- map.js
+++ map.js
@@ -13,3 +13,5 @@ const numbers = [1, 2, 3, 4, 5];
 const doubled = numbers.map(num => num * 2);
 const squared = numbers.map(num => num * num);
 console.log(squared)
+
+const friends = ['zaved', 'naved', 'khaled', 'sajed']
