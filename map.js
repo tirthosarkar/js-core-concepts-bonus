f6f49ce9e07e3fa2b00a554e894697be115daec5
tirthosarkar <tirthosarkar1@gmail.com>
2026-07-12 21:58:25 +0600

    added map.js const doubled = numbers.map(num => num * 2);

--- map.js
+++ map.js
@@ -1,9 +1,15 @@
 const numbers = [1, 2, 3, 4, 5];
 
-const doubled = [];
-for (const num of numbers) {
-  doubled.push(num * 2);
+// const doubled = [];
+// for (const num of numbers) {
+//   doubled.push(num * 2);
 
-}
+// }
 
+// const doubleIt = num => num * 2;
+// const doubled = numbers.map(doubleIt)
+
+
+const doubled = numbers.map(num => num * 2);
+const squared = numbers.map(num => num * num);
 console.log(doubled)
