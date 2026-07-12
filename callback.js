0c9aa895efd1f541f4ea1771128b6e0f527adf93
tirthosarkar <tirthosarkar1@gmail.com>
2026-07-12 12:46:27 +0600

    Added callback.js Kobul nari output

--- callback.js
+++ callback.js
@@ -1,4 +1,22 @@
-document.getElementById('btn').addEventListener('click', function (event) {
-});
+// document.getElementById('btn').addEventListener('click', function (event) {
+// });
 
-addEventListener('click', function(event){})
+// addEventListener('click', function (event) { })
+
+function settleLife(name, isBCS, marriage, parti) {
+  if (isBCS) {
+    marriage(parti);
+  }
+}
+
+// settleLife('Tomal', isBCS){
+//   if (isBCS)
+//     marriage(parti);
+// }
+
+function boloKobul(parti) {
+  console.log('Kobul', parti);
+}
+
+settleLife('Tomal', true, boloKobul, 'nari');  //Kobul nari output
+// boloKobul('pori')
