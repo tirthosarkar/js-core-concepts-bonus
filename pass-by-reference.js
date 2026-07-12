b2a148d1b1ca7bd8967b1bc707c9ac6f981a0d0e
tirthosarkar <tirthosarkar1@gmail.com>
2026-07-12 17:53:00 +0600

    Added pass-by-reference.js firstSum

--- pass-by-reference.js
+++ pass-by-reference.js
@@ -1,3 +1,16 @@
-function (a, b) {
-  
+function firstSum (arr1, arr2) {
+  // arr1[0] = 100;
+  // arr2[0] = 200;
+  const first = arr1[0];
+  arr2 second = arr2[0];
+  return first + second;
 }
+const num1 = [1, 2, 3];
+const num2 = [4, 5, 6];
+
+console.log('before the function call: ', num1, num2);
+
+const result = firstSum(num1, num2);
+
+console.log('after the function call:', num1, num2);
+
