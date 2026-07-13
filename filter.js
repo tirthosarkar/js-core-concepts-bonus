6379af857347fd0d1ce2075a56f0e04d72cf9ffd
tirthosarkar <tirthosarkar1@gmail.com>
2026-07-13 13:37:18 +0600

    Added filter.js NFriends students

--- filter.js
+++ filter.js
@@ -4,4 +4,13 @@ const evenNumbers = numbers.filter(num => num % 2 === 0);
 const friends = ['zaved', 'naved', 'khaled', 'sajed', 'nahid', 'nasib'];
 const NFriends = friends.filter(frd => frd[0] === 'n');
 
+const students = [
+  {name: 'Tormuj', age:22}
+  {name: 'Zaved', age:32}
+  {name: 'Naved', age:42}
+  {name: 'Khaled', age:52}
+  {name: 'Sajaed', age:62}
+  {name: 'Nahid', age:72}
+  {name: 'Nasib', age:82}
+]
 
