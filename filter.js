70220efc7e19adfec0079259c65c3599f14ac1ef
tirthosarkar <tirthosarkar1@gmail.com>
2026-07-13 13:12:39 +0600

    Added filter.js NFriends

--- filter.js
+++ filter.js
@@ -2,3 +2,6 @@ const numbers = [1, 2, 3, 4, 5];
 
 const evenNumbers = numbers.filter(num => num % 2 === 0);
 const friends = ['zaved', 'naved', 'khaled', 'sajed', 'nahid', 'nasib'];
+const NFriends = friends.filter(frd => frd[0] === 'n');
+
+
