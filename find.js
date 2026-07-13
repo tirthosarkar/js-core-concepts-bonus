7697c2323ab3bacf3b3b780adc2fa7a9993c8cf7
tirthosarkar <tirthosarkar1@gmail.com>
2026-07-13 18:30:28 +0600

    Added find.js

---
+++ find.js
@@ -0,0 +1,12 @@
+const students = [
+  { name: 'Tormuj', age: 22 },
+  { name: 'Zaved', age: 32 },
+  { name: 'Naved', age: 42 },
+  { name: 'Khaled', age: 52 },
+  { name: 'Sajaed', age: 62 },
+  { name: 'Nahid', age: 72 },
+  { name: 'Nasib', age: 82 },
+];
+
+const student = students.find(std => std.name === 'Khaled');
+console.log(student)
