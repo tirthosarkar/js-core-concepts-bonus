506dd742afa4447abf9967e901d1fccde6a12aad
tirthosarkar <tirthosarkar1@gmail.com>
2026-07-13 16:54:53 +0600

    Added filter.js NFriends students ,

--- filter.js
+++ filter.js
@@ -5,12 +5,14 @@ const friends = ['zaved', 'naved', 'khaled', 'sajed', 'nahid', 'nasib'];
 const NFriends = friends.filter(frd => frd[0] === 'n');
 
 const students = [
-  {name: 'Tormuj', age:22}
-  {name: 'Zaved', age:32}
-  {name: 'Naved', age:42}
-  {name: 'Khaled', age:52}
-  {name: 'Sajaed', age:62}
-  {name: 'Nahid', age:72}
-  {name: 'Nasib', age:82}
+  {name: 'Tormuj', age:22},
+  {name: 'Zaved', age:32},
+  {name: 'Naved', age:42},
+  {name: 'Khaled', age:52},
+  {name: 'Sajaed', age:62},
+  {name: 'Nahid', age:72},
+  {name: 'Nasib', age:82},
 ]
+
+console.log(NFriends);
 
